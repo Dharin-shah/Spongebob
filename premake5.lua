1be3ea544ec62fd9b38814eab88f0bf9bba279db
Dharin Shah <dharin.shah@outlook.com>
2019-10-13 19:11:10 +0530

    add premake build system for vs

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+-- premake5.lua
+
+workspace "Spongebob"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug", --debug , all code
+		"Release", -- Release with log but stripped off some code
+		"Dist" -- no log 
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	
+project "Spongebob"
+	location "Spongebob"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/client/**.h"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"SB_PLATFORM_WINDOWS",
+			"SB_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Patrick")
+		}
+
+
+	filter "configurations:Debug"
+		defines "SB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SB_DIST"
+		optimize "On"
+
+
+project "Patrick"
+	location "Patrick"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Spongebob/vendor/spdlog/include",
+		"Spongebob/client"
+	}
+
+	links
+	{
+		"Spongebob"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"SB_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SB_DIST"
+		optimize "On"
